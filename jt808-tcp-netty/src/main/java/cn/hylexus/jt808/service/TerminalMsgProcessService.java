267703cc03b1a7a04a29258f37a32842a8017fa1
mingdi <13817460364@163.com>
2017-07-17 16:03:49 +0800

    add GPS upload parse and basic information parsed. need to check the additional info after the first 16bits.

--- jt808-tcp-netty/src/main/java/cn/hylexus/jt808/service/TerminalMsgProcessService.java
+++ jt808-tcp-netty/src/main/java/cn/hylexus/jt808/service/TerminalMsgProcessService.java
@@ -1,5 +1,8 @@
 package cn.hylexus.jt808.service;
 
+import cn.hylexus.jt808.dao.GPSCreateDao;
+import cn.hylexus.jt808.entity.GPSEntity;
+import cn.hylexus.jt808.processor.impl.StandardGPSUploadProcessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,85 +17,96 @@ import cn.hylexus.jt808.vo.req.TerminalAuthenticationMsg;
 import cn.hylexus.jt808.vo.req.TerminalRegisterMsg;
 import cn.hylexus.jt808.vo.resp.ServerCommonRespMsgBody;
 import cn.hylexus.jt808.vo.resp.TerminalRegisterMsgRespBody;
+import java.nio.ByteBuffer;
 
 public class TerminalMsgProcessService extends BaseMsgProcessService {
+  
+  private final Logger log = LoggerFactory.getLogger(getClass());
+  private final Logger weblog = LoggerFactory.getLogger("weblog");
+  private MsgEncoder msgEncoder;
+  private SessionManager sessionManager;
+  
+  public TerminalMsgProcessService() {
+    this.msgEncoder = new MsgEncoder();
+    this.sessionManager = SessionManager.getInstance();
+  }
+  
+  public void processRegisterMsg(TerminalRegisterMsg msg) throws Exception {
+    log.debug("终端注册:{}", JSON.toJSONString(msg, true));
+    weblog.info("终端注册:{}", JSON.toJSONString(msg, true));
+    
+    final String sessionId = Session.buildId(msg.getChannel());
+    Session session = sessionManager.findBySessionId(sessionId);
+    if (session == null) {
+      session = Session.buildSession(msg.getChannel(), msg.getMsgHeader().getTerminalPhone());
+    }
+    session.setAuthenticated(true);
+    session.setTerminalPhone(msg.getMsgHeader().getTerminalPhone());
+    sessionManager.put(session.getId(), session);
+    
+    TerminalRegisterMsgRespBody respMsgBody = new TerminalRegisterMsgRespBody();
+    respMsgBody.setReplyCode(TerminalRegisterMsgRespBody.success);
+    respMsgBody.setReplyFlowId(msg.getMsgHeader().getFlowId());
+    // TODO 鉴权码暂时写死
+    respMsgBody.setReplyToken("123");
+    int flowId = super.getFlowId(msg.getChannel());
+    byte[] bs = this.msgEncoder.encode4TerminalRegisterResp(msg, respMsgBody, flowId);
+    
+    super.send2Client(msg.getChannel(), bs);
+  }
+  
+  public void processAuthMsg(TerminalAuthenticationMsg msg) throws Exception {
+    // TODO 暂时每次鉴权都成功
 
-	private final Logger log = LoggerFactory.getLogger(getClass());
-	private final Logger weblog = LoggerFactory.getLogger("weblog");
-	private MsgEncoder msgEncoder;
-	private SessionManager sessionManager;
+    log.debug("终端鉴权:{}", JSON.toJSONString(msg, true));
+    weblog.info("终端鉴权:{}", JSON.toJSONString(msg, true));
+    
+    final String sessionId = Session.buildId(msg.getChannel());
+    Session session = sessionManager.findBySessionId(sessionId);
+    if (session == null) {
+      session = Session.buildSession(msg.getChannel(), msg.getMsgHeader().getTerminalPhone());
+    }
+    session.setAuthenticated(true);
+    session.setTerminalPhone(msg.getMsgHeader().getTerminalPhone());
+    sessionManager.put(session.getId(), session);
+    
+    ServerCommonRespMsgBody respMsgBody = new ServerCommonRespMsgBody();
+    respMsgBody.setReplyCode(ServerCommonRespMsgBody.success);
+    respMsgBody.setReplyFlowId(msg.getMsgHeader().getFlowId());
+    respMsgBody.setReplyId(msg.getMsgHeader().getMsgId());
+    int flowId = super.getFlowId(msg.getChannel());
+    byte[] bs = this.msgEncoder.encode4ServerCommonRespMsg(msg, respMsgBody, flowId);
+    super.send2Client(msg.getChannel(), bs);
+  }
+  
+  public void processTerminalHeartBeatMsg(PackageData req) throws Exception {
+    log.debug("心跳信息:{}", JSON.toJSONString(req, true));
+    final MsgHeader reqHeader = req.getMsgHeader();
+    ServerCommonRespMsgBody respMsgBody = new ServerCommonRespMsgBody(reqHeader.getFlowId(), reqHeader.getMsgId(),
+            ServerCommonRespMsgBody.success);
+    int flowId = super.getFlowId(req.getChannel());
+    byte[] bs = this.msgEncoder.encode4ServerCommonRespMsg(req, respMsgBody, flowId);
+    super.send2Client(req.getChannel(), bs);
+  }
+  
+  public void processTerminalLogoutMsg(PackageData req) throws Exception {
+    log.info("终端注销:{}", JSON.toJSONString(req, true));
+    final MsgHeader reqHeader = req.getMsgHeader();
+    ServerCommonRespMsgBody respMsgBody = new ServerCommonRespMsgBody(reqHeader.getFlowId(), reqHeader.getMsgId(),
+            ServerCommonRespMsgBody.success);
+    int flowId = super.getFlowId(req.getChannel());
+    byte[] bs = this.msgEncoder.encode4ServerCommonRespMsg(req, respMsgBody, flowId);
+    super.send2Client(req.getChannel(), bs);
+  }
 
-	public TerminalMsgProcessService() {
-		this.msgEncoder = new MsgEncoder();
-		this.sessionManager = SessionManager.getInstance();
-	}
-
-	public void processRegisterMsg(TerminalRegisterMsg msg) throws Exception {
-		log.debug("终端注册:{}", JSON.toJSONString(msg, true));
-		weblog.info("终端注册:{}", JSON.toJSONString(msg, true));
-
-		final String sessionId = Session.buildId(msg.getChannel());
-		Session session = sessionManager.findBySessionId(sessionId);
-		if (session == null) {
-			session = Session.buildSession(msg.getChannel(), msg.getMsgHeader().getTerminalPhone());
-		}
-		session.setAuthenticated(true);
-		session.setTerminalPhone(msg.getMsgHeader().getTerminalPhone());
-		sessionManager.put(session.getId(), session);
-
-		TerminalRegisterMsgRespBody respMsgBody = new TerminalRegisterMsgRespBody();
-		respMsgBody.setReplyCode(TerminalRegisterMsgRespBody.success);
-		respMsgBody.setReplyFlowId(msg.getMsgHeader().getFlowId());
-		// TODO 鉴权码暂时写死
-		respMsgBody.setReplyToken("123");
-		int flowId = super.getFlowId(msg.getChannel());
-		byte[] bs = this.msgEncoder.encode4TerminalRegisterResp(msg, respMsgBody, flowId);
-
-		super.send2Client(msg.getChannel(), bs);
-	}
-
-	public void processAuthMsg(TerminalAuthenticationMsg msg) throws Exception {
-		// TODO 暂时每次鉴权都成功
-
-		log.debug("终端鉴权:{}", JSON.toJSONString(msg, true));
-		weblog.info("终端鉴权:{}", JSON.toJSONString(msg, true));
-
-		final String sessionId = Session.buildId(msg.getChannel());
-		Session session = sessionManager.findBySessionId(sessionId);
-		if (session == null) {
-			session = Session.buildSession(msg.getChannel(), msg.getMsgHeader().getTerminalPhone());
-		}
-		session.setAuthenticated(true);
-		session.setTerminalPhone(msg.getMsgHeader().getTerminalPhone());
-		sessionManager.put(session.getId(), session);
-
-		ServerCommonRespMsgBody respMsgBody = new ServerCommonRespMsgBody();
-		respMsgBody.setReplyCode(ServerCommonRespMsgBody.success);
-		respMsgBody.setReplyFlowId(msg.getMsgHeader().getFlowId());
-		respMsgBody.setReplyId(msg.getMsgHeader().getMsgId());
-		int flowId = super.getFlowId(msg.getChannel());
-		byte[] bs = this.msgEncoder.encode4ServerCommonRespMsg(msg, respMsgBody, flowId);
-		super.send2Client(msg.getChannel(), bs);
-	}
-
-	public void processTerminalHeartBeatMsg(PackageData req) throws Exception {
-		log.debug("心跳信息:{}", JSON.toJSONString(req, true));
-		final MsgHeader reqHeader = req.getMsgHeader();
-		ServerCommonRespMsgBody respMsgBody = new ServerCommonRespMsgBody(reqHeader.getFlowId(), reqHeader.getMsgId(),
-				ServerCommonRespMsgBody.success);
-		int flowId = super.getFlowId(req.getChannel());
-		byte[] bs = this.msgEncoder.encode4ServerCommonRespMsg(req, respMsgBody, flowId);
-		super.send2Client(req.getChannel(), bs);
-	}
-
-	public void processTerminalLogoutMsg(PackageData req) throws Exception {
-		log.info("终端注销:{}", JSON.toJSONString(req, true));
-		final MsgHeader reqHeader = req.getMsgHeader();
-		ServerCommonRespMsgBody respMsgBody = new ServerCommonRespMsgBody(reqHeader.getFlowId(), reqHeader.getMsgId(),
-				ServerCommonRespMsgBody.success);
-		int flowId = super.getFlowId(req.getChannel());
-		byte[] bs = this.msgEncoder.encode4ServerCommonRespMsg(req, respMsgBody, flowId);
-		super.send2Client(req.getChannel(), bs);
-	}
-
+  /**
+   * Terminal device location upload request processor
+   *
+   * @param req
+   * @throws Exception
+   */
+  public void processLocationUploadMsg(PackageData req, String deviceId) throws Exception {
+    StandardGPSUploadProcessor p = new StandardGPSUploadProcessor(deviceId);
+    p.process(req);
+  }
 }
--- jt808-tcp-netty/src/main/java/cn/hylexus/jt808/service/handler/TCPServerHandler.java
+++ jt808-tcp-netty/src/main/java/cn/hylexus/jt808/service/handler/TCPServerHandler.java
@@ -21,148 +21,153 @@ import io.netty.util.ReferenceCountUtil;
 
 public class TCPServerHandler extends ChannelInboundHandlerAdapter { // (1)
 
-	private final Logger logger = LoggerFactory.getLogger(getClass());
+  private final Logger logger = LoggerFactory.getLogger(getClass());
 
-	private final SessionManager sessionManager;
-	private final MsgDecoder decoder;
-	private TerminalMsgProcessService msgProcessService;
+  private final SessionManager sessionManager;
+  private final MsgDecoder decoder;
+  private TerminalMsgProcessService msgProcessService;
 
-	public TCPServerHandler() {
-		this.sessionManager = SessionManager.getInstance();
-		this.decoder = new MsgDecoder();
-		this.msgProcessService = new TerminalMsgProcessService();
-	}
+  public TCPServerHandler() {
+    this.sessionManager = SessionManager.getInstance();
+    this.decoder = new MsgDecoder();
+    this.msgProcessService = new TerminalMsgProcessService();
+  }
 
-	@Override
-	public void channelRead(ChannelHandlerContext ctx, Object msg) throws InterruptedException { // (2)
-		try {
-			ByteBuf buf = (ByteBuf) msg;
-			if (buf.readableBytes() <= 0) {
-				// ReferenceCountUtil.safeRelease(msg);
-				return;
-			}
+  @Override
+  public void channelRead(ChannelHandlerContext ctx, Object msg) throws InterruptedException { // (2)
+    try {
+      ByteBuf buf = (ByteBuf) msg;
+      if (buf.readableBytes() <= 0) {
+        // ReferenceCountUtil.safeRelease(msg);
+        return;
+      }
 
-			byte[] bs = new byte[buf.readableBytes()];
-			buf.readBytes(bs);
+      byte[] bs = new byte[buf.readableBytes()];
+      buf.readBytes(bs);
 
-			// 字节数据转换为针对于808消息结构的实体类
-			PackageData pkg = this.decoder.bytes2PackageData(bs);
-			// 引用channel,以便回送数据给硬件
-			pkg.setChannel(ctx.channel());
-			this.processPackageData(pkg);
-		} finally {
-			release(msg);
-		}
-	}
+      // 字节数据转换为针对于808消息结构的实体类
+      PackageData pkg = this.decoder.bytes2PackageData(bs);
+      // 引用channel,以便回送数据给硬件
+      pkg.setChannel(ctx.channel());
+      this.processPackageData(pkg);
+    } finally {
+      release(msg);
+    }
+  }
 
-	/**
-	 * 
-	 * 处理业务逻辑
-	 * 
-	 * @param packageData
-	 * 
-	 */
-	private void processPackageData(PackageData packageData) {
-		final MsgHeader header = packageData.getMsgHeader();
+  /**
+   *
+   * 处理业务逻辑
+   *
+   * @param packageData
+   *
+   */
+  private void processPackageData(PackageData packageData) {
+    final MsgHeader header = packageData.getMsgHeader();
 
-		// 1. 终端心跳-消息体为空 ==> 平台通用应答
-		if (TPMSConsts.msg_id_terminal_heart_beat == header.getMsgId()) {
-			logger.info(">>>>>[终端心跳],phone={},flowid={}", header.getTerminalPhone(), header.getFlowId());
-			try {
-				this.msgProcessService.processTerminalHeartBeatMsg(packageData);
-				logger.info("<<<<<[终端心跳],phone={},flowid={}", header.getTerminalPhone(), header.getFlowId());
-			} catch (Exception e) {
-				logger.error("<<<<<[终端心跳]处理错误,phone={},flowid={},err={}", header.getTerminalPhone(), header.getFlowId(),
-						e.getMessage());
-				e.printStackTrace();
-			}
-		}
+    // 1. 终端心跳-消息体为空 ==> 平台通用应答
+    if (TPMSConsts.msg_id_terminal_heart_beat == header.getMsgId()) {
+      logger.info(">>>>>[终端心跳],phone={},flowid={}", header.getTerminalPhone(), header.getFlowId());
+      try {
+        this.msgProcessService.processTerminalHeartBeatMsg(packageData);
+        logger.info("<<<<<[终端心跳],phone={},flowid={}", header.getTerminalPhone(), header.getFlowId());
+      } catch (Exception e) {
+        logger.error("<<<<<[终端心跳]处理错误,phone={},flowid={},err={}", header.getTerminalPhone(), header.getFlowId(),
+                e.getMessage());
+        e.printStackTrace();
+      }
+    } // 5. 终端鉴权 ==> 平台通用应答
+    else if (TPMSConsts.msg_id_terminal_authentication == header.getMsgId()) {
+      logger.info(">>>>>[终端鉴权],phone={},flowid={}", header.getTerminalPhone(), header.getFlowId());
+      try {
+        TerminalAuthenticationMsg authenticationMsg = new TerminalAuthenticationMsg(packageData);
+        this.msgProcessService.processAuthMsg(authenticationMsg);
+        logger.info("<<<<<[终端鉴权],phone={},flowid={}", header.getTerminalPhone(), header.getFlowId());
+      } catch (Exception e) {
+        logger.error("<<<<<[终端鉴权]处理错误,phone={},flowid={},err={}", header.getTerminalPhone(), header.getFlowId(),
+                e.getMessage());
+        e.printStackTrace();
+      }
+    } // 6. 终端注册 ==> 终端注册应答
+    else if (TPMSConsts.msg_id_terminal_register == header.getMsgId()) {
+      logger.info(">>>>>[终端注册],phone={},flowid={}", header.getTerminalPhone(), header.getFlowId());
+      try {
+        TerminalRegisterMsg msg = this.decoder.toTerminalRegisterMsg(packageData);
+        this.msgProcessService.processRegisterMsg(msg);
+        logger.info("<<<<<[终端注册],phone={},flowid={}", header.getTerminalPhone(), header.getFlowId());
+      } catch (Exception e) {
+        logger.error("<<<<<[终端注册]处理错误,phone={},flowid={},err={}", header.getTerminalPhone(), header.getFlowId(),
+                e.getMessage());
+        e.printStackTrace();
+      }
+    } // 7. 终端注销(终端注销数据消息体为空) ==> 平台通用应答
+    else if (TPMSConsts.msg_id_terminal_log_out == header.getMsgId()) {
+      logger.info(">>>>>[终端注销],phone={},flowid={}", header.getTerminalPhone(), header.getFlowId());
+      try {
+        this.msgProcessService.processTerminalLogoutMsg(packageData);
+        logger.info("<<<<<[终端注销],phone={},flowid={}", header.getTerminalPhone(), header.getFlowId());
+      } catch (Exception e) {
+        logger.error("<<<<<[终端注销]处理错误,phone={},flowid={},err={}", header.getTerminalPhone(), header.getFlowId(),
+                e.getMessage());
+        e.printStackTrace();
+      }
+    } else if (TPMSConsts.msg_id_terminal_location_info_upload == header.getMsgId()) {
+      logger.info(">>>>>[终端注销],phone={},flowid={}", header.getTerminalPhone(), header.getFlowId());
+      try {
+        this.msgProcessService.processLocationUploadMsg(packageData, header.getTerminalPhone());
+      } catch (Exception e) {
+        logger.error("<<<<<[GPS数据上传]处理错误,phone={},flowid={},err={}", header.getTerminalPhone(), header.getFlowId(),
+                e.getMessage());
+        e.printStackTrace();
+        e.printStackTrace();
+      }
+    } // 其他情况
+    else {
+      logger.error(">>>>>>[未知消息类型],phone={},msgId={},package={}", header.getTerminalPhone(), header.getMsgId(),
+              packageData);
+    }
+  }
 
-		// 5. 终端鉴权 ==> 平台通用应答
-		else if (TPMSConsts.msg_id_terminal_authentication == header.getMsgId()) {
-			logger.info(">>>>>[终端鉴权],phone={},flowid={}", header.getTerminalPhone(), header.getFlowId());
-			try {
-				TerminalAuthenticationMsg authenticationMsg = new TerminalAuthenticationMsg(packageData);
-				this.msgProcessService.processAuthMsg(authenticationMsg);
-				logger.info("<<<<<[终端鉴权],phone={},flowid={}", header.getTerminalPhone(), header.getFlowId());
-			} catch (Exception e) {
-				logger.error("<<<<<[终端鉴权]处理错误,phone={},flowid={},err={}", header.getTerminalPhone(), header.getFlowId(),
-						e.getMessage());
-				e.printStackTrace();
-			}
-		}
-		// 6. 终端注册 ==> 终端注册应答
-		else if (TPMSConsts.msg_id_terminal_register == header.getMsgId()) {
-			logger.info(">>>>>[终端注册],phone={},flowid={}", header.getTerminalPhone(), header.getFlowId());
-			try {
-				TerminalRegisterMsg msg = this.decoder.toTerminalRegisterMsg(packageData);
-				this.msgProcessService.processRegisterMsg(msg);
-				logger.info("<<<<<[终端注册],phone={},flowid={}", header.getTerminalPhone(), header.getFlowId());
-			} catch (Exception e) {
-				logger.error("<<<<<[终端注册]处理错误,phone={},flowid={},err={}", header.getTerminalPhone(), header.getFlowId(),
-						e.getMessage());
-				e.printStackTrace();
-			}
-		}
-		// 7. 终端注销(终端注销数据消息体为空) ==> 平台通用应答
-		else if (TPMSConsts.msg_id_terminal_log_out == header.getMsgId()) {
-			logger.info(">>>>>[终端注销],phone={},flowid={}", header.getTerminalPhone(), header.getFlowId());
-			try {
-				this.msgProcessService.processTerminalLogoutMsg(packageData);
-				logger.info("<<<<<[终端注销],phone={},flowid={}", header.getTerminalPhone(), header.getFlowId());
-			} catch (Exception e) {
-				logger.error("<<<<<[终端注销]处理错误,phone={},flowid={},err={}", header.getTerminalPhone(), header.getFlowId(),
-						e.getMessage());
-				e.printStackTrace();
-			}
-		}
-		// 其他情况
-		else {
-			logger.error(">>>>>>[未知消息类型],phone={},msgId={},package={}", header.getTerminalPhone(), header.getMsgId(),
-					packageData);
-		}
-	}
+  @Override
+  public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+    logger.error("发生异常:{}", cause.getMessage());
+    cause.printStackTrace();
+  }
 
-	@Override
-	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
-		logger.error("发生异常:{}", cause.getMessage());
-		cause.printStackTrace();
-	}
+  @Override
+  public void channelActive(ChannelHandlerContext ctx) throws Exception {
+    Session session = Session.buildSession(ctx.channel());
+    sessionManager.put(session.getId(), session);
+    logger.debug("终端连接:{}", session);
+  }
 
-	@Override
-	public void channelActive(ChannelHandlerContext ctx) throws Exception {
-		Session session = Session.buildSession(ctx.channel());
-		sessionManager.put(session.getId(), session);
-		logger.debug("终端连接:{}", session);
-	}
+  @Override
+  public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+    final String sessionId = ctx.channel().id().asLongText();
+    Session session = sessionManager.findBySessionId(sessionId);
+    this.sessionManager.removeBySessionId(sessionId);
+    logger.debug("终端断开连接:{}", session);
+    ctx.channel().close();
+    // ctx.close();
+  }
 
-	@Override
-	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-		final String sessionId = ctx.channel().id().asLongText();
-		Session session = sessionManager.findBySessionId(sessionId);
-		this.sessionManager.removeBySessionId(sessionId);
-		logger.debug("终端断开连接:{}", session);
-		ctx.channel().close();
-		// ctx.close();
-	}
+  @Override
+  public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+    if (IdleStateEvent.class.isAssignableFrom(evt.getClass())) {
+      IdleStateEvent event = (IdleStateEvent) evt;
+      if (event.state() == IdleState.READER_IDLE) {
+        Session session = this.sessionManager.removeBySessionId(Session.buildId(ctx.channel()));
+        logger.error("服务器主动断开连接:{}", session);
+        ctx.close();
+      }
+    }
+  }
 
-	@Override
-	public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
-		if (IdleStateEvent.class.isAssignableFrom(evt.getClass())) {
-			IdleStateEvent event = (IdleStateEvent) evt;
-			if (event.state() == IdleState.READER_IDLE) {
-				Session session = this.sessionManager.removeBySessionId(Session.buildId(ctx.channel()));
-				logger.error("服务器主动断开连接:{}", session);
-				ctx.close();
-			}
-		}
-	}
-
-	private void release(Object msg) {
-		try {
-			ReferenceCountUtil.release(msg);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+  private void release(Object msg) {
+    try {
+      ReferenceCountUtil.release(msg);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
